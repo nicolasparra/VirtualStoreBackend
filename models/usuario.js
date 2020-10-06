51179767cdd6ff3adc1d9be14cca2fb68172d335
nicolas1008 <n.parra04@ufromail.cl>
2020-10-06 14:40:13 -0300

    correcion modelos

--- models/usuario.js
+++ models/usuario.js
@@ -6,9 +6,9 @@ const usuario = Schema({
     nombre : {type:String,required:true,maxlength:100},
     foto:{type:String},
     token:{type:String,select:false},
-    email:{type:String,unique:true,maxlength:100},
+    email:{type:String,maxlength:100},
     password:{type:String,required:true,select:false},
-    telefono:{type:Number,unique:true,maxlength:15},
+    telefono:{type:Number,maxlength:15},
     items:[{type: mongoose.Schema.Types.ObjectId, ref: 'item'}]
 });
 
